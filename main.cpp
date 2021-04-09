0feeda92a4644160edcf6060a63358ff29486552
Nyzus <drzetein@hotmail.com>
2021-04-09 16:13:23 -0300

    simplified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 
-// Get target system from compiler argument -D _<OS>_BUILD_
-// These macros with _surrounding_underscores_ are only meant to be defined in the compiler
-// checking for them in code may lead to undefined behaviour
+/**
+ * Get target system from the compiler-defined macro _<OS>_BUILD_
+ * 
+ * These macros with _surrounding_underscores_ are only meant to be defined in the compiler
+ * checking for them in code may lead to undefined behaviour
+ *
+ * Therefore, use only LINUX_BUILD or WINDOWS_BUILD to define OS specific instructions
+ **/
 
-// Therefore, use only LINUX_BUILD or WINDOWS_BUILD to define OS specific instructions
 #define LINUX_BUILD
 #define WINDOWS_BUILD
 
@@ -20,17 +24,13 @@
 using namespace std;
 
 int main(int argc, char** argv=(char**)" ")
-{
-    // Read version file
+{   // Read version file
     string ProgramPath;
-    if (argc) { ProgramPath = argv[0]; }
+    if (argc) ProgramPath = argv[0];
     ifstream version(ProgramPath + ".version", ios::in);
     if (version.is_open()) {
-        while(char c = version.get()) {
-            if (c!=-1) {
-                cout << c;
-            }
-        }
+        while(char c = version.get())
+            if (c!=-1) cout << c;
         version.close();
         return 0;
     }
