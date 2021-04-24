aeab7c9632eb185dc94ce26b10d65dd719d00314
drzetein <drzetein@hotmail.com>
2021-04-23 22:50:03 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
 #include <fstream>
 
-/**
- * Get target system from the compiler-defined macro _<OS>_BUILD_
- * 
- * These macros with _surrounding_underscores_ are only meant to be defined in the compiler
- * checking for them in code may lead to undefined behaviour
- *
- * Therefore, use only LINUX_BUILD or WINDOWS_BUILD to define OS specific instructions
- **/
-
-#define LINUX_BUILD
-#define WINDOWS_BUILD
-
-#ifdef _LINUX_BUILD_
-    #undef WINDOWS_BUILD
-#endif
-
-#ifdef _WINDOWS_BUILD_
-    #undef LINUX_BUILD
-#endif
-
 using namespace std;
 
 int main(int argc, char** argv)
